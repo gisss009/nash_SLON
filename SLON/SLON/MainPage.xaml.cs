9c3b70c29aba5052d84e46ab1e3f70d3096e7598
Рустам Уджуху <diamantove@mail.ru>
2024-11-18 12:43:22 +0300

    edit MainPage
    + card
    + title

--- SLON/SLON/MainPage.xaml.cs
+++ SLON/SLON/MainPage.xaml.cs
@@ -1,13 +1,30 @@
-﻿namespace SLON
+﻿using SLON.Models;
+using System.Collections.ObjectModel;
+
+namespace SLON
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<User> Users { get; set; } = new();
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
+        protected async override void OnNavigatedTo(NavigatedToEventArgs args)
+        {
+            base.OnNavigatedTo(args);
+
+            Users.Clear();
+
+            for (int i = 0; i <= 50; i++)
+            {
+                User user = new(i, "Name #" + i, "tags", "vocation", "info", "skills");
+                Users.Add(user); 
+            }
+        }
 
     }
-
 }
