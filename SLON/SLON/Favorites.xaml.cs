b6702f423f4e6d648819ea7c3a7d9ba2ef65588b
Продащук Вадим <vadosprodashik@gmail.com>
2025-03-14 22:04:14 +0300

    Changed favorites #2
    Разрешение конфликтов

--- SLON/SLON/Favorites.xaml.cs
+++ SLON/SLON/Favorites.xaml.cs
@@ -6,37 +6,18 @@ using System.Linq;
 
 namespace SLON
 {
-<<<<<<< Updated upstream
-    public ObservableCollection<User> favorites = Favourites.favorites;
-    private Image starIcon;
-
-    public Favorites()
-=======
     public partial class Favorites : ContentPage
->>>>>>> Stashed changes
     {
-        // Режим отображения: Events (true) или Profiles (false)
+        // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: Events (true) пїЅпїЅпїЅ Profiles (false)
         private bool showingEvents = true;
-        // Для профилей: All (true) или Mutual (false)
+        // пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: All (true) пїЅпїЅпїЅ Mutual (false)
         private bool showingAll = true;
 
         private ObservableCollection<LikeItem> LikeItems { get; set; } = new();
 
         public bool IsChatAvailable => !showingEvents && !showingAll;
 
-<<<<<<< Updated upstream
-        LoadFavorites();
-    }
-
-    private void LoadFavorites()
-    {
-        Debug.WriteLine($"count posle: {favorites.Count}");
-        favoritesPanel.Children.Clear();
-        //Image starIcon;
-        if (favorites.Count == 0)
-=======
         public Favorites()
->>>>>>> Stashed changes
         {
             InitializeComponent();
             BindingContext = this;
@@ -95,7 +76,7 @@ namespace SLON
 
 
 
-        #region Переключатели
+        #region пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
         private void OnAllClicked(object sender, EventArgs e)
         {
@@ -131,7 +112,7 @@ namespace SLON
 
         #endregion
 
-        #region Колокольчик
+        #region пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
         private async void OnBellClicked(object sender, EventArgs e)
         {
@@ -148,77 +129,6 @@ namespace SLON
             {
                 if (item.IsEvent && item.EventData != null)
                 {
-<<<<<<< Updated upstream
-                    Text = $"{user.Name}\n{user.Vocation}",
-                    TextColor = Colors.White,
-                    FontSize = 16,
-                    VerticalOptions = LayoutOptions.Center,
-                    HorizontalOptions = LayoutOptions.Start
-                };
-                Grid.SetColumn(userLabel, 1);
-
-                starIcon = new Image
-                {
-                    Source = ImageSource.FromFile("Resources/Images/empty_star.png"),
-                    WidthRequest = 50,
-                    HeightRequest = 50,
-                    VerticalOptions = LayoutOptions.Center,
-                    HorizontalOptions = LayoutOptions.End
-                };
-                Grid.SetColumn(starIcon, 1);
-                
-
-                userGrid.Children.Add(userIcon);
-                userGrid.Children.Add(userLabel);
-                userGrid.Children.Add(starIcon);
-
-                TapGestureRecognizer tapGesture = new TapGestureRecognizer();
-                tapGesture.Tapped += (s, e) => OnUserTapped(user);
-                userGrid.GestureRecognizers.Add(tapGesture);
-
-                userFrame.Content = userGrid;
-
-                favoritesPanel.Children.Add(userFrame);
-            }
-        }
-    }
-
-    private async void OnUserTapped(User selectedUser)
-    {
-        // Получаем starIcon через родительский элемент, чтобы изменить его источник
-        var userFrame = favoritesPanel.Children
-            .OfType<Frame>()
-            .FirstOrDefault(f => f.Content is Grid userGrid && userGrid.Children.OfType<Label>().Any(l => l.Text.Contains(selectedUser.Name)));
-
-        if (userFrame != null)
-        {
-            var userGrid = (Grid)userFrame.Content;
-            var starIcon = userGrid.Children.OfType<Image>().LastOrDefault(); // Получаем последний элемент, который является starIcon
-
-            if (starIcon != null)
-            {
-                // Меняем источник изображения
-                if (starIcon.Source.ToString().Contains("empty_star.png"))
-                {
-                    starIcon.Source = ImageSource.FromFile("Resources/Images/fill_star.png");
-                }
-                else
-                {
-                    starIcon.Source = ImageSource.FromFile("Resources/Images/empty_star.png");
-                }
-            }
-        }
-
-        //await DisplayAlert("User Info", $"You clicked on {selectedUser.Name}", "OK");
-    }
-
-    //private async void OnUserTapped(User selectedUser)
-    //{
-
-    //    await DisplayAlert("User Info", $"You clicked on {selectedUser.Name}", "OK");
-    //}
-};
-=======
                     Favourites.favoriteEvents.Remove(item.EventData);
                     LikeItems.Remove(item);
                 }
@@ -240,7 +150,7 @@ namespace SLON
                     var ev = item.EventData;
                     if (!ev.IsPublic)
                     {
-                        await DisplayAlert("Event", "Ивент приватный, нельзя добавить участников", "OK");
+                        await DisplayAlert("Event", "пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ", "OK");
                         return;
                     }
                     var popup = new AddUsersToEventPopup(ev);
@@ -250,11 +160,10 @@ namespace SLON
                 {
                     if (!IsChatAvailable)
                         return;
-                    await DisplayAlert("Chat", $"Открыт чат с {item.Title}", "OK");
+                    await DisplayAlert("Chat", $"пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅ {item.Title}", "OK");
                 }
             }
         }
->>>>>>> Stashed changes
 
         private async void OnItemTapped(object sender, EventArgs e)
         {
@@ -267,7 +176,7 @@ namespace SLON
                 }
                 else if (!selectedItem.IsEvent && selectedItem.UserData != null)
                 {
-                    await DisplayAlert("User Info", $"Просмотр профиля: {selectedItem.UserData.Name}", "OK");
+                    await DisplayAlert("User Info", $"пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ: {selectedItem.UserData.Name}", "OK");
                 }
             }
         }
