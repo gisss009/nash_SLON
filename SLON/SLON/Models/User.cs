4a6e644e3b6fe346d2a75f5ea6c5646efd5491c8
Продащук Вадим <vadosprodashik@gmail.com>
2025-03-14 20:19:42 +0300

    Changed favorites #1
    Добавление переключателей, заглушек для работы с профилями/ивентами, просмотра карточки ивента, принятие/просмотр запросов от пользователей.

--- SLON/SLON/Models/User.cs
+++ SLON/SLON/Models/User.cs
@@ -1,28 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SLON.Models;
-
-public class User
+﻿namespace SLON.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Tags { get; set; }
-    public string Vocation { get; set; }
-    public string Info { get; set; }
-    public string Skills { get; set; }
-
-    public User(int id, string name, string tags, string vocation, string info, string skills)
+    public class User
     {
-        Id = id;
-        Name = name;
-        Tags = tags;
-        Vocation = vocation;
-        Info = info;
-        Skills = skills;
+        public int Id { get; set; }
+        public string Name { get; set; }       
+        public string Tags { get; set; }       
+        public string Vocation { get; set; }   
+        public string Info { get; set; }     
+        public string Skills { get; set; }     
+
+        public bool IsMutual { get; set; }     // Если true - во "взаимных"
+        public bool IsAcceptedMe { get; set; } // Принял ли он меня
+        public bool IsILikedHim { get; set; }  // Лайкнул ли я его
+
+        public User(int id, string name, string tags, string vocation, string info, string skills)
+        {
+            Id = id;
+            Name = name;
+            Tags = tags;
+            Vocation = vocation;
+            Info = info;
+            Skills = skills;
+            IsMutual = false;
+            IsAcceptedMe = false;
+            IsILikedHim = false;
+        }
     }
 }
-
