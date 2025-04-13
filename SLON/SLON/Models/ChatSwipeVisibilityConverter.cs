092b9666f5bb8cedf8910a2ee2de277f60dbe4a3
Продащук Вадим <vadosprodashik@gmail.com>
2025-04-13 12:42:24 +0300

    Update Favorites
    Свайп влево отключен для пользователей из All.

---
+++ SLON/SLON/Models/ChatSwipeVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace SLON.Models
+{
+    public class ChatSwipeVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2)
+                return true;
+
+            bool isEvent = (values[0] is bool b) && b;
+            bool isChatAvailable = (values[1] is bool chat) && chat;
+
+            return isEvent || isChatAvailable;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
