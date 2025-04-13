827b20534f432f0dff948362a6360069d8879301
Продащук Вадим <vadosprodashik@gmail.com>
2025-04-13 11:51:57 +0300

    Update MainActivity.cs
    отключение альбомной ориентации

--- SLON/SLON/Platforms/Android/MainActivity.cs
+++ SLON/SLON/Platforms/Android/MainActivity.cs
@@ -3,9 +3,12 @@ using Android.Content.PM;
 
 namespace SLON
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme",
+              MainLauncher = true,
+              LaunchMode = LaunchMode.SingleTop,
+              ScreenOrientation = ScreenOrientation.Portrait,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-
     }
 }
